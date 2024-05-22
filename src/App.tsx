bf1912f2760f4476ab4b83745e7ef4854b0d5a22
DmitriyShkredov <dmitriyshkredovdev@gmail.com>
2024-05-22 22:16:32 +0300

    feat: add game

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import classnames from "classnames";
+import { getEmptyXY, canMoveItem, genRandomwArr, correctArr } from "./util";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [state, setState] = useState(genRandomwArr());
+
+  const moveItem = (x: number, y: number, item: number) => {
+    const [emptyX, emptyY] = getEmptyXY(state, 16);
+    if (canMoveItem(emptyX, emptyY, x, y)) {
+      setState(() => {
+        const newState = new Array(...state);
+        newState[y][x] = 16;
+        newState[emptyY][emptyX] = item;
+        return newState;
+      });
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="game">
+        {state.map((line, y) =>
+          line.map((item, x) => {
+            return (
+              <div
+                className={classnames(
+                  "game-item",
+                  correctArr[y][x] === item && "success",
+                  item === 16 && "empty"
+                )}
+                style={{ top: y * 25 + "%", left: x * 25 + "%" }}
+                key={item}
+                onClick={() => moveItem(x, y, item)}
+              >
+                {item}
+              </div>
+            );
+          })
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button
+        className="reset-button"
+        onClick={() => setState(genRandomwArr())}
+      >
+        reset
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/util.ts
@@ -0,0 +1,54 @@
+export const getEmptyXY = (
+  array: Array<number[]>,
+  number: number
+): Array<number> => {
+  let emptyX = 1;
+  let emptyY = 1;
+  array.forEach((line, y) => {
+    line.forEach((item, x) => {
+      if (item === number) {
+        emptyX = x;
+        emptyY = y;
+      }
+    });
+  });
+  return [emptyX, emptyY];
+};
+
+export const canMoveItem = (
+  emptyX: number,
+  emptyY: number,
+  x: number,
+  y: number
+) => {
+  if (emptyX === x && Math.abs(emptyY - y) === 1) return true;
+  if (emptyY === y && Math.abs(emptyX - x) === 1) return true;
+  return false;
+};
+
+export const genRandomwArr = () => {
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  let m = arr.length;
+  let t;
+  let i;
+  while (m) {
+    i = Math.floor(Math.random() * m--);
+    t = arr[m];
+    arr[m] = arr[i];
+    arr[i] = t;
+  }
+
+  return [
+    arr.slice(0, 4),
+    arr.slice(4, 8),
+    arr.slice(8, 12),
+    arr.slice(12, 16),
+  ];
+};
+
+export const correctArr = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, 16],
+];
